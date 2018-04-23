e71ef5907ab49e9430b9294b540b7f68ffdf22f5
NoHeal4u <miodragnikolic@yahoo.com>
2018-04-23 21:00:24 +0200

    first commit

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div>
+
+  <table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Email</th>
+  </tr>
+
+  <tr  v-for="(user,key) in users" :key="key">
+      <td>{{user.name}}</td>
+      <td>{{user.lastName}}</td>
+      <td>{{user.email}}</td>
+  </tr>
+  
+</table>
+
+  </div>
+
+
+</template>
+
+
+<script>
+
+
+export default {
+
+  name : 'ContactsList',
+
+  data(){
+
+    return{
+
+      users: [
+      { name: 'Pera' , lastName: 'Peric' , email: 'peraperic@gmail.com'},
+      { name: 'Sima' , lastName: 'Simic' , email: 'simasimic@gmail.com'},
+      { name: 'Laza' , lastName: 'Lazic' , email: 'lazalazic@gmail.com'}
+
+        ]
+
+    }
+  }
+}
+
+</script>
+
